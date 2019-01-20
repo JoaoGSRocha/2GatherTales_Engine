b19797df988aacf8183cc94ab0a8a0437e68b5f1
JoaoGSRocha <joaogsrocha@gmail.com>
2019-01-20 00:11:39 +0000

    [Add] JSONParser, Needed Parser Model Classes & ButtonFactory
    - Adds Reponse(master node), Key(1st node), Answer,Question,Trigger(Optional Nodes);
    - Adds JSONParser;
    - Adds ButtonFactory;
    
    NOTES: Next commit pass UI_Logic createButton() to ButtonFactory();
    
    Keywords: Refactor, Game2D, UI_Elements, Modular
    Importance: Medium

---
+++ core/src/com/mygdx/game/Model/Parser/Response.java
@@ -0,0 +1,34 @@
+package com.mygdx.game.Model.Parser;
+
+import java.util.ArrayList;
+
+public class Response {
+
+    private Key key;
+    private ArrayList<Answer> answer_al;
+    private ArrayList<Question> question_al;
+    private ArrayList<Trigger> trigger_al;
+
+    public Key getKey() { return key; }
+
+    public void setKey(Key key) { this.key = key; }
+
+    public ArrayList<Answer> getAnswer_al() { return answer_al; }
+
+    public void setAnswer_al(ArrayList<Answer> answer_al) {
+        this.answer_al = answer_al;
+    }
+
+    public ArrayList<Question> getQuestion_al() { return question_al; }
+
+    public void setQuestion_al(ArrayList<Question> question_al) {
+        this.question_al = question_al;
+    }
+
+    public ArrayList<Trigger> getTrigger_al() { return trigger_al; }
+
+    public void setTrigger_al(ArrayList<Trigger> trigger_al) {
+        this.trigger_al = trigger_al;
+    }
+
+}
