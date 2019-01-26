346fc8a2b57781577b3fc405b9c5bd0a1d01156d
alexandre <alexandre@alexandre.home>
2019-01-26 17:57:55 +0000

    Add Menu Music and FadeOut effect when exit

--- core/src/com/mygdx/game/UI/MenuScreens.java
+++ core/src/com/mygdx/game/UI/MenuScreens.java
@@ -11,6 +11,7 @@ import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.audio.*;
 import com.mygdx.game.Game2D;
 import com.mygdx.game.JSON.JSONParser;
 
@@ -18,7 +19,8 @@ public class MenuScreens implements Screen {
 
     private Game2D parent;
     private Stage stage;
-
+    private boolean exitFadeOut=false;
+    public Sound sound = Gdx.audio.newSound(Gdx.files.internal("sounds/intro menu storytaler punchline.mp3"));
     Skin skin = new Skin(Gdx.files.internal("flat/skin/skin.json"));
 
     public MenuScreens(Game2D game) {
@@ -26,6 +28,11 @@ public class MenuScreens implements Screen {
 
         stage = new Stage(new ScreenViewport());
         Gdx.input.setInputProcessor(stage);
+
+        sound.play();
+        sound.setLooping(0,true);
+        sound.setVolume(0, parent.gameSoundVolume);
+
     }
 
     @Override
@@ -70,7 +77,8 @@ public class MenuScreens implements Screen {
         exit.addListener(new ChangeListener() {
             @Override
             public void changed(ChangeEvent event, Actor actor) {
-                Gdx.app.exit();
+                //Gdx.app.exit();
+                exitFadeOut=true;
             }
         });
 
@@ -121,8 +129,9 @@ public class MenuScreens implements Screen {
         audioSlider.addListener(new ChangeListener() {
             @Override
             public void changed(ChangeEvent event, Actor actor) {
-                //pbrShader.albedoColor.x=audioSlider.getValue();
+                //pbrShader.albedoColor.x-=audioSlider.getValue();
                 parent.gameSoundVolume=audioSlider.getValue();
+                sound.setVolume(0, parent.gameSoundVolume);
             }
         });
     }
@@ -132,8 +141,16 @@ public class MenuScreens implements Screen {
         Gdx.gl.glClearColor(0f, 0f, 0f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
-        stage.draw();
+        if(exitFadeOut){
+            parent.gameSoundVolume-=0.01f;
+            sound.setVolume(0,parent.gameSoundVolume);
+            if(parent.gameSoundVolume<0){
+                Gdx.app.exit();
+            }
+        }else{
+            stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
+            stage.draw();
+        }
     }
 
     @Override
